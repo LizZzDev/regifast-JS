5e5ad1affffaa910b76152634152ea510cc99928
LizZzDev <lizethcruz181112@gmail.com>
2025-04-24 22:33:58 -0600

    chambea mas

--- practicas-back/src/controladores/alumnos/barraStatus.js
+++ practicas-back/src/controladores/alumnos/barraStatus.js
@@ -1,6 +1,13 @@
 const barraStatus = {
     obtenerBarraStatusPorId: async (id) => {
-    
+        try {
+            const barraStatus = await Alumno.obtenerAlumno(id);
+            console.log ("barra", barraStatus);
+            return barraStatus;
+        } catch (error) {
+            console.error("Error al obtener la barra de status:", error);
+            throw error;
+        }
     },
 
     aumentarBarraStatusPorId: async (id) => {
--- practicas-back/src/controladores/alumnos/index.js
+++ practicas-back/src/controladores/alumnos/index.js
@@ -1,8 +1,7 @@
 import anadirDatosDelAlumnoService from "./anadirDatosDelAlumno.js";
+import barraStatusService from "./barraStatus.js";
 
 export const anadirDatosDelAlumno = async (req, res) => {
-  console.log("Sesión completa:", req.session);
-
     try {
       const response = await anadirDatosDelAlumnoService(req);
       res.status(201).json({ mensaje: "Alumno agregado correctamente", response });
@@ -11,4 +10,26 @@ export const anadirDatosDelAlumno = async (req, res) => {
       return res.status(401).json({ success: false, message: error.message });
     }
   };
+  
+export const obtenerBarraStatus = async (req, res) => {
+    try {
+      const response = await barraStatusService.obtenerBarraStatusPorId(req);
+      res.status(201).json({ mensaje: "Se obtuvo el paso donde se ubica el alumno (barra status)", response });
+    } catch (error) {
+      console.error("Error al obtener la cifra:", error.message);
+      return res.status(401).json({ success: false, message: error.message });
+    }
+  };
+
+
+export const aumentarBarraStatusPorId = async (req, res) => {
+    try {
+      const response = await barraStatusService.aumentarBarraStatusPorId(req);
+      res.status(201).json({ mensaje: "Se aumento la barra satatus", response });
+    } catch (error) {
+      console.error("Error al aumentar:", error.message);
+      return res.status(401).json({ success: false, message: error.message });
+    }
+  };
+  
   
