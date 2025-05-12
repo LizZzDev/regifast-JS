6bca092a568cb7ed7b39fde004ce9e3d39420760
LizZzDev <lizethcruz181112@gmail.com>
2025-05-11 18:11:22 -0600

    m

--- practicas-front-end/src/App.jsx
+++ practicas-front-end/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import RegistroEmpresa from './paginas/empresas/registroEmpresa';
 import Login from './paginas/usuarios/login';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro-empresa" element={<RegistroEmpresa />} />
+      </Routes>
+    </Router>
   );
 }
 
