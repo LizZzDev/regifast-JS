8b4634d486fb83e0881e8bde277c3adfd575e2c5
LizZzDev <lizethcruz181112@gmail.com>
2025-05-15 22:25:46 -0600

    yes

--- practicas-front-end/src/paginas/jefeDepto/crearJefeDepto.jsx
+++ practicas-front-end/src/paginas/jefeDepto/crearJefeDepto.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./nuevoAdmin.css";
+import "./crearJefeDepto.css";
 
 function CrearNuevoAdmin() {
   const [formData, setFormData] = useState({
--- practicas-front-end/src/rutas/RutasJefe.jsx
+++ practicas-front-end/src/rutas/RutasJefe.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import PrincipalJefe from '../paginas/jefeDepto/principalJefe';
+import TablaAlumnos from '../paginas/jefeDepto/tablaAlumnos';
 
 const RutasJefe = () => (
     <Routes>
+        <Route path="/" element={<PrincipalJefe />} />
+        <Route path="/tabla-alumnos" element={<TablaAlumnos />} />
     </Routes>
 );
 
