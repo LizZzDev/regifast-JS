a8786d4a4c1e40d4f055c367359a130e7ca7b71d
yan-bs <yanethcarrillo32@gmail.com>
2025-05-20 12:51:36 -0600

    arregle cositas

--- practicas-front-end/src/paginas/alumnos/principalAlumno.jsx
+++ practicas-front-end/src/paginas/alumnos/principalAlumno.jsx
@@ -99,7 +99,7 @@ const BarraStatus = () => {
           <section id="InformacionBarraEstado">
             <p>
               Tu proceso se encuentra en: <br />
-              <span>{textosEstado[progreso]}</span>.
+              <span>{textosEstado[progreso]}</span>
             </p>
           </section>
         </article>
